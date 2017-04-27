4c42ecee174bb130dd5e62f9a4a60747e2966261
wangchonghui2219 <wangchonghui2219@126.com>
2017-04-27 09:24:38 +0800

    Initial commit

---
+++ utileslibrary/src/main/java/com/example/utileslibrary/TimeUtiles.java
@@ -0,0 +1,42 @@
+package com.example.utileslibrary;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @作者 wch
+ * @create at
+ * @name 时间转换
+ */
+
+public class TimeUtiles {
+
+    // 将字符串转为时间戳
+
+    public static String getTime(String user_time) {
+        String re_time = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日HH时mm分ss秒");
+        Date d;
+        try {
+            d = sdf.parse(user_time);
+            long l = d.getTime();
+            String str = String.valueOf(l);
+            re_time = str.substring(0, 10);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return re_time;
+    }
+
+    // 将时间戳转为字符串
+    public static String getStrTime(String cc_time) {
+        String re_StrTime = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMM");
+        // 例如：cc_time=1291778220
+        long lcc_time = Long.valueOf(cc_time);
+        re_StrTime = sdf.format(new Date(lcc_time * 1000L));
+        return re_StrTime;
+    }
+}
